0f5704add49d70c7eb07b09cd45b4832c49070c2
Ayaya <tea23511131719@yahoo.co.jp>
2016-10-26 15:53:01 +0900

    fuck

--- app/src/main/java/com/example/ayaya/myapplication19/MainActivity.java
+++ app/src/main/java/com/example/ayaya/myapplication19/MainActivity.java
@@ -1,20 +1,57 @@
 package com.example.ayaya.myapplication19;
 
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ListViewCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.image.SmartImageView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private TweetAdapter mAdapter;
+    private Twitter mTwitter;
+    private ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (!TwitterUtils.hasAccessToken(this)) {
+            Intent intent = new Intent(this, TwitterOAuthActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            mAdapter = new TweetAdapter(this);
+            setListAdapter(mAdapter);
+            reloadTimeLine();
+            mTwitter = TwitterUtils.getTwitterInstance(this);
+        }
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,5 +85,61 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void reloadTimeLine() {
+        AsyncTask<Void, Void, List<twitter4j.Status>> task = new AsyncTask<Void, Void, List<twitter4j.Status>>() {
+            @Override
+            protected List<twitter4j.Status> doInBackground(Void... params) {
+                try {
+                    return mTwitter.getHomeTimeline();
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(List<twitter4j.Status> result) {
+                if (result != null) {
+                    mAdapter.clear();
+                    for (twitter4j.Status status : result) {
+                        mAdapter.add(status);
+                    }
+                    getListView().setSelection(0);
+                } else {
+                    showToast("タイムラインの取得に失敗しました。。。");
+                }
+            }
+        };
+        task.execute();
+    }
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+}
+private class TweetAdapter extends ArrayAdapter<Status> {
+
+    private LayoutInflater mInflater;
+
+    public TweetAdapter(Context context) {
+        super(context, android.R.layout.simple_list_item_1);
+        mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.list_item_tweet, null);
+        }
+        Status item = getItem(position);
+        TextView name = (TextView) convertView.findViewById(R.id.name);
+        name.setText(item.getUser().getName());
+        TextView screenName = (TextView) convertView.findViewById(R.id.screen_name);
+        screenName.setText("@" + item.getUser().getScreenName());
+        TextView text = (TextView) convertView.findViewById(R.id.text);
+        text.setText(item.getText());
+        SmartImageView icon = (SmartImageView) convertView.findViewById(R.id.icon);
+        icon.setImageUrl(item.getUser().getProfileImageURL());
+        return convertView;
     }
 }
