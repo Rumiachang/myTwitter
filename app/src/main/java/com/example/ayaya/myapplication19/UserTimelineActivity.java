38987b3ed365dd71d4a76e4411f62ed45d11fa86
Ayaya <tea23511131719@yahoo.co.jp>
2016-12-16 14:51:04 +0900

    いろいろと？くだんのjavascriptのHtmlファイルをアセットとした？

--- app/src/main/java/com/example/ayaya/myapplication19/UserTimelineActivity.java
+++ app/src/main/java/com/example/ayaya/myapplication19/UserTimelineActivity.java
@@ -37,6 +37,7 @@ FragmentOfUserTimeLineList.OnFragmentInteractionListener, FragmentOfUsersFavorit
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
+            //a
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
